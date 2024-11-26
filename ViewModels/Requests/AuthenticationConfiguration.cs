cce5f9786cca0f96bd8dd696196d07c7a8b2c426
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-11-27 00:34:08 +0530

    Authentication Binded

---
+++ ViewModels/Requests/AuthenticationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace EMS_Project.ViewModels.Requests
+{
+    public class AuthenticationConfiguration
+    {
+        public string AccessTokenSecret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessTokenExpirationMinutes { get; set; }
+    }
+}
